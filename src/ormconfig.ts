4144fcca0d36494c8872a854bc1bef59462d5866
Sergey Serzhan <serzhansergey96@gmail.com>
2022-04-26 17:53:50 +0300

    deploy on heroku

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -8,14 +8,13 @@ dotenv.config({
 
 export default {
   type: 'postgres',
-  host: 'db',
-  port: parseInt(process.env.POSTGRES_PORT as string, 10) as number,
-  cache: false,
-  username: process.env.POSTGRES_USER as string,
-  password: process.env.POSTGRES_PASSWORD as string,
-  database: process.env.POSTGRES_DB as string,
-  synchronize: false,
-  logging: false,
+  // host: 'ec2-52-48-159-67.eu-west-1.compute.amazonaws.com',
+  // port: 5432,
+  url: 'postgres://spolgmafitdrhk:628e8523dba34e5f5d7ad0491c2978d18d6d922cb60ab92bdfbf594589c8b18c@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d5j06e0s3qc8vs',
+  username: 'spolgmafitdrhk',
+  password: '628e8523dba34e5f5d7ad0491c2978d18d6d922cb60ab92bdfbf594589c8b18c',
+  // database: 'd5j06e0s3qc8vs',
   entities: ['src/resources/**/**.entity{.ts,.js}'],
   migrations: ['./migrations/*.ts'],
+  ssl: { rejectUnauthorized: false }
 } as ConnectionOptions;
